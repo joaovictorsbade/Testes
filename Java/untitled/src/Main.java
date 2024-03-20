bc48d74601439aae7edb65bdbadd59c779c094db
joaovictorsbade <125564144+joaovictorsbade@users.noreply.github.com>
2024-03-20 18:21:12 -0300

    Add files via upload

---
+++ Java/untitled/src/Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //--Chamar Métodos e Funções--
+        //LacoDeRepeticao();
+        //ScannerInput();
+
+    }
+
+    //Laço de Repetição
+    public static void LacoDeRepeticao(){
+        System.out.println("Laço de Repetição");
+        for (int i = 1 ; 1 <= 5 ; i++){
+            System.out.println("i = " + i);
+        }
+    }//Laço de Repetição
+
+    //Scanner
+    public static void ScannerInput() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite um texto: ");
+        String texto = scanner.nextLine();
+        System.out.println("Você digitou: " + texto);
+        scanner.close();
+    }//Scanner
+
+
+
+}
